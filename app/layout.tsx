5e0481d774a2233969b5493734b5f10044a62fe6
Jonatan <barakawins@gmail.com>
2025-03-27 07:50:50 -0300

    fav-icon.

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: config.seo.title,
-  description: config.seo.description
+  description: config.seo.description,
 };
 
 export default function RootLayout({
@@ -32,6 +32,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+	<link 
+	  rel="icon"
+	  href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${config.seo.favIcon}</text></svg>`}
+	/>
 	<link
 	  rel="preload"
 	  href={config.profile.picture}
